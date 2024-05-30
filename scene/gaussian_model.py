0f35624bb0fedc90d7b153d45a5d9c85eef3722a
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2024-05-30 18:54:53 +0200

    gaussian model: pruning into square grid, sorting, neighbor loss

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -458,3 +458,151 @@ class GaussianModel:
     def add_densification_stats(self, viewspace_point_tensor, update_filter):
         self.xyz_gradient_accum[update_filter] += torch.norm(viewspace_point_tensor.grad[update_filter,:2], dim=-1, keepdim=True)
         self.denom[update_filter] += 1
+
+
+    # SSGS implementation
+
+    def prune_all_but_these_indices(self, indices):
+
+        if self.optimizer is not None:
+
+            optimizable_tensors = self._prune_optimizer(indices)
+
+            self._xyz = optimizable_tensors["xyz"]
+            self._features_dc = optimizable_tensors["f_dc"]
+            self._features_rest = optimizable_tensors["f_rest"]
+            self._opacity = optimizable_tensors["opacity"]
+            self._scaling = optimizable_tensors["scaling"]
+            self._rotation = optimizable_tensors["rotation"]
+
+            self.xyz_gradient_accum = self.xyz_gradient_accum[indices]
+            self.denom = self.denom[indices]
+            self.max_radii2D = self.max_radii2D[indices]
+        else:
+            self._xyz = self._xyz[indices]
+            self._features_dc = self._features_dc[indices]
+            self._features_rest = self._features_rest[indices]
+            self._opacity = self._opacity[indices]
+            self._scaling = self._scaling[indices]
+            self._rotation = self._rotation[indices]
+
+
+    def prune_to_square_shape(self, sort_by_opacity=True, verbose=True):
+        num_gaussians = self._xyz.shape[0]
+
+        self.grid_sidelen = int(np.sqrt(num_gaussians))
+        num_removed = num_gaussians - self.grid_sidelen * self.grid_sidelen
+
+        if verbose:
+            print(f"Removing {num_removed}/{num_gaussians} gaussians to fit the grid. ({100 * num_removed / num_gaussians:.4f}%)")
+        if self.grid_sidelen * self.grid_sidelen < num_gaussians:
+            if sort_by_opacity:
+                alpha = self.get_opacity[:, 0]
+                _, keep_indices = torch.topk(alpha, k=self.grid_sidelen * self.grid_sidelen)
+            else:
+                shuffled_indices = torch.randperm(num_gaussians)
+                keep_indices = shuffled_indices[:self.grid_sidelen * self.grid_sidelen]
+            sorted_keep_indices = torch.sort(keep_indices)[0]
+            self.prune_all_but_these_indices(sorted_keep_indices)
+
+    @staticmethod
+    def normalize(tensor):
+        tensor = tensor - tensor.mean()
+        if tensor.std() > 0:
+            tensor = tensor / tensor.std()
+        return tensor
+
+    def sort_into_grid(self, sorting_cfg, verbose):
+        
+        normalization_fn = self.normalize if sorting_cfg.normalize else lambda x: x
+        attr_getter_fn = self.get_activated_attr_flat if sorting_cfg.activated else self.get_attr_flat
+        
+        params_to_sort = []
+        
+        for attr_name, attr_weight in sorting_cfg.weights.items():
+            if attr_weight > 0:
+                params_to_sort.append(normalization_fn(attr_getter_fn(attr_name)) * attr_weight)
+                    
+        params_to_sort = torch.cat(params_to_sort, dim=1)
+        
+        if sorting_cfg.shuffle:
+            shuffled_indices = torch.randperm(params_to_sort.shape[0], device=params_to_sort.device)
+            params_to_sort = params_to_sort[shuffled_indices]
+
+        grid_to_sort = self.as_grid_img(params_to_sort).permute(2, 0, 1)
+        _, sorted_indices = sort_with_plas(grid_to_sort, improvement_break=sorting_cfg.improvement_break, verbose=verbose)
+        
+        sorted_indices = sorted_indices.squeeze().flatten()
+        
+        if sorting_cfg.shuffle:
+            sorted_indices = shuffled_indices[sorted_indices]
+        
+        self.prune_all_but_these_indices(sorted_indices)
+
+    def as_grid_img(self, tensor):
+        if not hasattr(self, "grid_sidelen"):
+            raise "Gaussians not pruned yet!"
+
+        if self.grid_sidelen * self.grid_sidelen != tensor.shape[0]:
+            raise "Tensor shape does not match img sidelen, needs pruning?"
+
+        img = tensor.reshape((self.grid_sidelen, self.grid_sidelen, -1))
+        return img
+
+    def attr_as_grid_img(self, attr_name):
+        tensor = getattr(self, attr_name)
+        return self.as_grid_img(tensor)
+
+    def set_attr_from_grid_img(self, attr_name, img):
+
+        if self.optimizer is not None:
+            raise "Overwriting Gaussians during training not implemented yet! - Consider pruning method implementations"
+
+        attr_shapes = {
+            "_xyz": (3,),
+            "_features_dc": (1, 3),
+            "_features_rest": ((self.max_sh_degree + 1) ** 2 - 1, 3),
+            "_rotation": (4,),
+            "_scaling": (3,),
+            "_opacity": (1,),
+        }
+
+        target_shape = attr_shapes[attr_name]
+        img_shaped = img.reshape(-1, *target_shape)
+        tensor = torch.tensor(img_shaped, dtype=torch.float, device="cuda")
+
+        setattr(self, attr_name, tensor)
+
+    def neighborloss_2d(self, tensor, neighbor_cfg, squeeze_dim=None):
+        if neighbor_cfg.normalize:
+            tensor = self.normalize(tensor)
+
+        if squeeze_dim:
+            tensor = tensor.squeeze(squeeze_dim)
+
+        img = self.as_grid_img(tensor)
+        img = img.permute(2, 0, 1).unsqueeze(0)
+
+        blurred_x = kornia.filters.gaussian_blur2d(
+            img.detach(),
+            kernel_size=(1, neighbor_cfg.blur.kernel_size),
+            sigma=(neighbor_cfg.blur.sigma, neighbor_cfg.blur.sigma),
+            border_type="circular",
+        )
+
+        blurred_xy = kornia.filters.gaussian_blur2d(
+            blurred_x,
+            kernel_size=(neighbor_cfg.blur.kernel_size, 1),
+            sigma=(neighbor_cfg.blur.sigma, neighbor_cfg.blur.sigma),
+            border_type="reflect",
+        )
+
+        if neighbor_cfg.loss_fn == "mse":
+            loss = F.mse_loss(blurred_xy, img)
+        elif neighbor_cfg.loss_fn == "huber":
+            loss = F.huber_loss(blurred_xy, img)
+        else:
+            assert False, "Unknown loss function"
+        
+        return loss
+
